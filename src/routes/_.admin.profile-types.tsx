6eb1daf5c163e0f5d6e63b154fc95808ebe2ae24
Zechariah Mahler <zechy.mahler@gmail.com>
2026-09-20 16:49:59 -0600

    fix: get profile type id from server

--- src/routes/_.admin.profile-types.tsx
+++ src/routes/_.admin.profile-types.tsx
@@ -12,7 +12,9 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
   const { data } = await client
     .from('profile_types')
-    .select('name,display_name,order,image_url,small_image_url,description,map_pin_name,is_space')
+    .select(
+      'id,name,display_name,order,image_url,small_image_url,description,map_pin_name,is_space',
+    )
     .order('order');
 
   const profileTypes = (data || []).map((profile_type) =>
